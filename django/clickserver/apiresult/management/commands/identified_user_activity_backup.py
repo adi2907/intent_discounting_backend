4a36820a368cbe735b9d90504cb31f09254518f1
root <root@ip-172-31-36-47.ap-south-1.compute.internal>
2023-07-24 12:47:28 +0000

    identified user compilation

--- django/clickserver/apiresult/management/commands/identified_user_activity_backup.py
+++ django/clickserver/apiresult/management/commands/identified_user_activity_backup.py
@@ -15,62 +15,47 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         end_time = datetime.now()
         start_time = end_time - timedelta(days=1)
-
-        # Get all IdentifiedUsers, regardless of their logged_time
-        identified_users = IdentifiedUser.objects.all()
+        
+        identified_users = IdentifiedUser.objects.filter(logged_time__gte=start_time, logged_time__lte=end_time)
 
         # Prepare the data for pandas DataFrame
         data = {
             'Name': [],
             'Phone': [],
             'Email': [],
-            'Registered User ID': [],
-            'Registered Date': [],
             'Product Visits': [],
             'Product Carts': []
         }
-
+        
         for identified_user in identified_users:
             user_name = identified_user.name
             user_phone = identified_user.phone
             user_email = identified_user.email
-            registered_user_id = identified_user.registered_user_id
-            registered_date = identified_user.logged_time
-
-            if identified_user.registered_user_id.strip() != '':
+            
+            if identified_user.registered_user_id and identified_user.registered_user_id.strip() != '':
                 try:
-                    user = User.objects.filter(registered_user_id=identified_user.registered_user_id).first()
-                    if user is None:
-                        logging.info(f"No User exists with registered_user_id: {identified_user.registered_user_id}")
-                        continue
+                    user = User.objects.get(registered_user_id=identified_user.registered_user_id)
                     user_email = user.user_login
-                except Exception as e:
-                    logging.error(f"An error occurred: {str(e)}")
+                except User.DoesNotExist:
+                    logging.info(f"No User exists with registered_user_id: {identified_user.registered_user_id}")
                     continue
 
             tokens = identified_user.tokens
             users = User.objects.filter(token__in=tokens)
-            # Fetch only visits from the last 24 hours
             visits = Visits.objects.filter(user__in=users, created_at__gte=start_time, created_at__lte=end_time)
-
+            
             product_visits = list(visits.values('item__product_id', 'item__name').annotate(visit_count=Count('item')).order_by('-visit_count'))
             product_visits = json.dumps(product_visits).replace(',', '|')
 
-            # Fetch only carts from the last 24 hours
             carts = Cart.objects.filter(user__in=users, created_at__gte=start_time, created_at__lte=end_time)
             product_carts = carts.values('item__product_id', 'item__name')
-            product_carts_list = list(product_carts)  # Convert queryset to a list of dictionaries
-            product_carts = json.dumps(product_carts_list).replace(',', '|')  # Pass the list of dictionaries to json.dumps
+            product_carts = list(set((item['item__product_id'], item['item__name']) for item in product_carts))
+            product_carts = json.dumps(product_carts).replace(',', '|')
 
-            # if both product_visits and product_carts are empty, skip this user
-            if product_visits == '[]' and product_carts == '[]':
-                continue
             # Append the user data
             data['Name'].append(user_name)
             data['Phone'].append(user_phone)
             data['Email'].append(user_email)
-            data['Registered User ID'].append(registered_user_id)
-            data['Registered Date'].append(registered_date.strftime("%Y-%m-%d"))
             data['Product Visits'].append(product_visits)
             data['Product Carts'].append(product_carts)
 
