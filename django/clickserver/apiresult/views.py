435e3567b50ee15e0f89fa5fd8e3dda98b47512f
adi2907 <adityaganguli@gmail.com>
2023-11-02 14:23:13 +0530

    api changes for app_name

--- django/clickserver/apiresult/views.py
+++ django/clickserver/apiresult/views.py
@@ -10,97 +10,55 @@ import random
 
 
 ''' returns carts for user
-Usage: curl "http://127.0.0.1:8000/api/carts/?token=q6wxm4v47y9"
-curl "http://127.0.0.1:8000/api/carts/?token=q6wxm4v47y9&last_7_days=True"
-curl "http://127.0.0.1:8000/api/carts/?token=q6wxm4v47y9&last_7_days&max_items=5"
-curl  "http://127.0.0.1:8000/api/carts/?token=q6wxm4v47y9&start_date=2023-01-01&end_date=2023-01-02?max_items=5"
-Response: ["114338","130233","130438","114799","128072"]
+Usage: "almeapp.com/api/carts/?token=q6wxm4v47y9&max_items=5&app_name=test_shopify"
 '''
 class CartView(APIView):
     
     # return cart items for user
     def get(self, request):
         token = self.request.query_params.get('token', None)
-        start_date = self.request.query_params.get('start_date', None)
-        end_date = self.request.query_params.get('end_date', None)
-        last_7_days = self.request.query_params.get('last_7_days', None)
-        last_30_days = self.request.query_params.get('last_30_days', None)
+        app_name = self.request.query_params.get('app_name', None)
+        if token or app_name is None: # respond with error
+            return Response({'error': 'token and app_name must be specified'})
         max_items = self.request.query_params.get('max_items', 10)
-
-        if last_7_days is not None:
-            queryset = Cart.objects.filter(user__token=token, created_at__gte=timezone.now() - timezone.timedelta(days=7))
-        elif last_30_days is not None:
-            queryset = Cart.objects.filter(user__token=token, created_at__gte=timezone.now() - timezone.timedelta(days=30))
-        elif start_date is not None and end_date is not None:
-            queryset = Cart.objects.filter(user__token=token, created_at__range=(start_date, end_date))
-        else:
-            queryset = Cart.objects.filter(user__token=token)
+        
+        # default query is for last 7 days, filter by token and app_name
+        queryset = Cart.objects.filter(user__token=token, app_name=app_name, created_at__gte=timezone.now() - timezone.timedelta(days=7))
         queryset = queryset.values('item__product_id').annotate(count=Count('item__product_id')).order_by('-count')[:int(max_items)]
         product_ids = [Item.objects.get(product_id=item['item__product_id']).product_id for item in queryset]
         return Response(product_ids)
 
 ''' returns visits for user in descending order of number of visits
-Usage: curl "http://127.0.0.1:8000/api/visits/?token=q6wxm4v47y9"
-curl "http://127.0.0.1:8000/api/visits/?token=q6wxm4v47y9&last_7_days=True"
-curl "http://127.0.0.1:8000/api/visits/?token=q6wxm4v47y9&last_7_days&max_items=5"
-curl  "http://127.0.0.1:8000/api/visits/?token=q6wxm4v47y9&start_date=2023-01-01&end_date=2023-01-02?max_items=5"
-Response: ["130233","128072","130438","107096","114799"]
+Usage: "almeapp.com/api/visits/?token=q6wxm4v47y9&app_name=test_shopify&max_items=5"
 '''
 class VisitsView(APIView):
     def get(self, request):
         token = self.request.query_params.get('token', None)
-        start_date = self.request.query_params.get('start_date', None)
-        end_date = self.request.query_params.get('end_date', None)
-        last_7_days = self.request.query_params.get('last_7_days', None)
-        last_30_days = self.request.query_params.get('last_30_days', None)
+        app_name = self.request.query_params.get('app_name', None)
+        if token or app_name is None: # respond with error
+            return Response({'error': 'token and app_name must be specified'})
         max_items = self.request.query_params.get('max_items', 10)
-
-        if last_7_days is not None:
-            queryset = Visits.objects.filter(user__token=token, created_at__gte=timezone.now() - timezone.timedelta(days=7))
-        elif last_30_days is not None:
-            queryset = Visits.objects.filter(user__token=token, created_at__gte=timezone.now() - timezone.timedelta(days=30))
-        elif start_date is not None and end_date is not None:
-            queryset = Visits.objects.filter(user__token=token, created_at__range=(start_date, end_date))
-        else:
-            # default to 7 days if no date range is specified
-            queryset = Visits.objects.filter(user__token=token, created_at__gte=timezone.now() - timezone.timedelta(days=7))
+        queryset = Visits.objects.filter(user__token=token,app_name=app_name, created_at__gte=timezone.now() - timezone.timedelta(days=7))
         
         # Annotate visit count and order by descending count
-        queryset = queryset.values('item__product_id').annotate(visit_count=Count('item__product_id')).order_by('-visit_count')
+        queryset = queryset.values('item__product_id').annotate(visit_count=Count('item__product_id')).order_by('-visit_count')[:int(max_items)]
         product_ids = [Item.objects.get(product_id=item['item__product_id']).product_id for item in queryset]
         return Response(product_ids)
  
 
 '''
-Returns most visited items
-Usage: curl "http://127.0.0.1:8000/api/most_visited/?max_items=5"
-Response: ["127109","130239","126319","130438","129470"]
+Returns most visited items for the shop
+Usage: "almeapp.com/api/most_visited/?app_name=test_shopify"
 '''
 
 class MostVisitedView(APIView):
     def get(self,request):
-        start_date = self.request.query_params.get('start_date', None)
-        end_date = self.request.query_params.get('end_date', None)
-        last_7_days = self.request.query_params.get('last_7_days', None)
-        last_30_days = self.request.query_params.get('last_30_days', None)
-        # set max_items to 30 if not specified
-        max_items = self.request.query_params.get('max_items', 30)
-       
-
-
-        if last_7_days is not None:
-            queryset = Visits.objects.filter(created_at__gte=timezone.now() - timezone.timedelta(days=7))
-        elif last_30_days is not None:
-            queryset = Visits.objects.filter(created_at__gte=timezone.now() - timezone.timedelta(days=30))
-        elif start_date is not None and end_date is not None:
-            queryset = Visits.objects.filter(created_at__range=(start_date, end_date))
-        else:
-            # default to 7 days if no date range is specified
-            queryset = Visits.objects.filter(created_at__gte=timezone.now() - timezone.timedelta(days=7))
-        
+        app_name = self.request.query_params.get('app_name', None)
+        if app_name is None: # respond with error
+            return Response({'error': 'app_name must be specified'})
             
+        queryset = Visits.objects.filter(app_name=app_name, created_at__gte=timezone.now() - timezone.timedelta(days=7))
         queryset = queryset.values('item__product_id').annotate(count=Count('item__product_id')).order_by('-count')
-        queryset = queryset[:int(max_items)].values('item__product_id') 
        
         product_ids = []
         for item in queryset:
@@ -110,36 +68,24 @@ class MostVisitedView(APIView):
         return Response(product_ids)
 '''   
 Returns most carted items
-Usage: curl "http://127.0.0.1:8000/api/most_carted/?max_items=5"
-Response: ["130188","130415","128734","128153","124324"]
+Usage: "almeapp.com/api/most_carted/?app_name=test_shopify"
 '''
 class MostCartedView(APIView):
     def get(self,request):
-        start_date = self.request.query_params.get('start_date', None)
-        end_date = self.request.query_params.get('end_date', None)
-        last_7_days = self.request.query_params.get('last_7_days', None)
-        last_30_days = self.request.query_params.get('last_30_days', None)
-        max_items = self.request.query_params.get('max_items', 30)
-
-        if last_7_days is not None:
-            queryset = Cart.objects.filter(created_at__gte=timezone.now() - timezone.timedelta(days=7))
-        elif last_30_days is not None:
-            queryset = Cart.objects.filter(created_at__gte=timezone.now() - timezone.timedelta(days=30))
-        elif start_date is not None and end_date is not None:
-            queryset = Cart.objects.filter(created_at__range=(start_date, end_date))
-        else:
-            # default to 7 days if no date range is specified
-            queryset = Cart.objects.filter(created_at__gte=timezone.now() - timezone.timedelta(days=7))
-
+        app_name = self.request.query_params.get('app_name', None)
+        if app_name is None: # respond with error
+            return Response({'error': 'app_name must be specified'})
+            
+        queryset = Cart.objects.filter(app_name=app_name, created_at__gte=timezone.now() - timezone.timedelta(days=7))
         queryset = queryset.values('item__product_id').annotate(count=Count('item__product_id')).order_by('-count')
-        queryset = queryset[:int(max_items)].values('item__product_id')
-        
+       
         product_ids = []
         for item in queryset:
             product_ids.append(Item.objects.get(product_id=item['item__product_id']).product_id)
         # randomly select 10 items from product_ids
         product_ids = random.sample(product_ids, min(len(product_ids), 10))
         return Response(product_ids)
+
 
 # check if user is new user
 class NewUserCheckView(APIView):
