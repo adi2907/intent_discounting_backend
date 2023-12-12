f711c2ae169fe026fcf0e9e4cc49600b768ba496
adi2907 <adityaganguli@gmail.com>
2023-12-12 16:37:02 +0530

    corrected VisitsView

--- django/clickserver/apiresult/views.py
+++ django/clickserver/apiresult/views.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 ''' returns carts for user
-Usage: "almeapp.com/api/crowd_favorites/?token=q6wxm4v47y9&max_items=5&app_name=almestore1.myshopify.com"
+Usage: "almeapp.com/api/crowd_favorites/?token=539311cienx&app_name=almestore1.myshopify.com"
 '''
 class CartView(APIView):
     
@@ -31,24 +31,23 @@ class CartView(APIView):
         return Response(product_ids)
 
 ''' returns visits for user in descending order of number of visits
-Usage: "almeapp.com/api/featured_collection/?token=q6wxm4v47y9&app_name=almestore1.myshopify.com"
+Usage: "almeapp.com/api/featured_collection/?token=539311cienx&app_name=almestore1.myshopify.com"
 '''
+
 class VisitsView(APIView):
+    def get(self, request):
+        token = request.query_params.get('token', None)
+        app_name = request.query_params.get('app_name', None)
+        if not token or not app_name: 
+            return Response({'error': 'token and app_name must be specified'})
 
-    class VisitsView(APIView):
-        def get(self, request):
-            token = request.query_params.get('token', None)
-            app_name = request.query_params.get('app_name', None)
-            if not token or not app_name: 
-                return Response({'error': 'token and app_name must be specified'})
-
-            max_items = request.query_params.get('max_items', 10)
-            queryset = Visits.objects.filter(user__token=token,app_name=app_name, created_at__gte=timezone.now() - timezone.timedelta(days=7))
-            
-            # Annotate visit count and order by descending count
-            queryset = queryset.values('item__product_id').annotate(visit_count=Count('item__product_id')).order_by('-visit_count')[:int(max_items)]
-            product_ids = [Item.objects.get(product_id=item['item__product_id']).product_id for item in queryset]
-            return Response(product_ids)
+        max_items = request.query_params.get('max_items', 10)
+        queryset = Visits.objects.filter(user__token=token,app_name=app_name, created_at__gte=timezone.now() - timezone.timedelta(days=7))
+        
+        # Annotate visit count and order by descending count
+        queryset = queryset.values('item__product_id').annotate(visit_count=Count('item__product_id')).order_by('-visit_count')[:int(max_items)]
+        product_ids = [Item.objects.get(product_id=item['item__product_id']).product_id for item in queryset]
+        return Response(product_ids)
  
 
 '''
@@ -68,7 +67,7 @@ class MostVisitedView(APIView):
         product_ids = []
         for item in queryset:
             product_ids.append(Item.objects.get(product_id=item['item__product_id']).product_id)
-        logger.info(product_ids)
+        
         # randomly select 10 items from product_ids
         product_ids = random.sample(product_ids, min(len(product_ids), 10))
         return Response(product_ids)
