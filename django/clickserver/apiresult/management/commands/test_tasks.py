8fb0e0431a2acadb4408718e4608d548f02c06ba
adi2907 <adityaganguli@gmail.com>
2024-02-08 07:33:50 +0530

    test tasks debug 3

--- django/clickserver/apiresult/management/commands/test_tasks.py
+++ django/clickserver/apiresult/management/commands/test_tasks.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     help = 'Test aggregation of users, visits, and sessions in 15-minute intervals'
 
     def handle(self, *args, **kwargs):
-
+        logger.info("Testing aggregation")
         # Define the 15-minute interval
         end_time_events = datetime.now() - timedelta(minutes=5)
         start_time_events = end_time_events - timedelta(minutes=10)
@@ -28,7 +28,7 @@ class Command(BaseCommand):
             events = Event.objects.filter(app_name=app_name,click_time__gte=start_time_events,click_time__lte=end_time_events)
 
             user_tokens_events = set(events.values_list('token', flat=True).distinct())
-            user_tokens_apiresult = set(IdentifiedUser.objects.filter(app_name=app_name,logged_time__gte=start_time_apiresult,logged_time__lte=end_time_apiresult).values_list('tokens', flat=True).distinct())
+            user_tokens_apiresult = set(User.objects.filter(app_name=app_name,logged_time__gte=start_time_apiresult,logged_time__lte=end_time_apiresult).values_list('tokens', flat=True).distinct())
 
             if not user_tokens_events.issubset(user_tokens_apiresult):
                 missing_tokens = user_tokens_events - user_tokens_apiresult
@@ -36,7 +36,7 @@ class Command(BaseCommand):
         
             # get list of all sessions and check if they are in Sessions model
             sessions_events = set(events.values_list('session', flat=True).distinct())
-            sessions_apiresult = set(Sessions.objects.filter(app_name=app_name,session_start__gte=start_time_apiresult,session_start__lte=end_time_apiresult).values_list('session_key', flat=True).distinct())
+            sessions_apiresult = set(Sessions.objects.filter(app_name=app_name,logged_time_gte=start_time_apiresult,logged_time_lte=end_time_apiresult).values_list('session_key', flat=True).distinct())
             
             if not sessions_events.issubset(sessions_apiresult):
                 missing_sessions = sessions_events - sessions_apiresult
@@ -46,7 +46,7 @@ class Command(BaseCommand):
             # tally the number of visits
             # visit is where event_type is 'page_load' and has a product_id
             visits_events = events.filter(event_type='page_load',product_id__isnull=False)
-            visits_apiresult = Visits.objects.filter(app_name=app_name,created_at__gte=start_time_events,created_at__lte=end_time_events)
+            visits_apiresult = Visits.objects.filter(app_name=app_name,created_at__gte=start_time_apiresult,created_at__lte=end_time_apiresult)
 
             if visits_events.count() != visits_apiresult.count():
                 logger.info(f"Number of visits in Visits model for app {app_name} does not match number of visits in events model")
@@ -55,7 +55,7 @@ class Command(BaseCommand):
             # tally the number of carts
             # cart is where click_text is 'add to cart'
             carts_events = events.filter(click_text='add to cart')
-            carts_apiresult = Cart.objects.filter(app_name=app_name,created_at__gte=start_time_events,created_at__lte=end_time_events)
+            carts_apiresult = Cart.objects.filter(app_name=app_name,created_at__gte=start_time_apiresult,created_at__lte=end_time_apiresult)
             if carts_events.count() != carts_apiresult.count():
                 logger.info(f"Number of carts in Cart model for app {app_name} does not match number of carts in events model")
                 logger.info(f"Number of carts in events model: {carts_events.count()} and in Cart model: {carts_apiresult.count()}")
