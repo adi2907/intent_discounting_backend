c4b03bb23e2c0f567eb5d1ffaae9df763ed018c6
adi2907 <adityaganguli@gmail.com>
2024-02-29 16:55:54 +0530

    unique session fix 4

--- django/clickserver/apiresult/tasks.py
+++ django/clickserver/apiresult/tasks.py
@@ -222,10 +222,9 @@ def get_session_variables(session_events,app_name):
     
     # update session
     session_start = min(session_events, key=lambda e: e['click_time'])['click_time']
-    
-    session_start = datetime.strptime(session_start, '%Y-%m-%dT%H:%M:%S.%f')
+    session_start = datetime.strptime(session_start, '%Y-%m-%dT%H:%M:%S')
     session_end = max(session_events, key=lambda e: e['click_time'])['click_time']
-    session_end = datetime.strptime(session_end, '%Y-%m-%dT%H:%M:%S.%f')
+    session_end = datetime.strptime(session_end, '%Y-%m-%dT%H:%M:%S')
     
 
     events_count = len(session_events)
