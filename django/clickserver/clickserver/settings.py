d3430adf4ed8b1ec0bd50cba0a561e67a23e88c3
adi2907 <adityaganguli@gmail.com>
2024-03-06 23:17:56 +0530

    error fix in utils config

--- django/clickserver/clickserver/settings.py
+++ django/clickserver/clickserver/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from apiresult.config import TIME_CHUNK
+from apiresult.utils.config import TIME_CHUNK
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
