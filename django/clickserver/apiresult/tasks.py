2ff80bdcf294cae892a08620b9cd2ea63c223fd5
adi2907 <adityaganguli@gmail.com>
2024-03-16 16:06:28 +0530

    moved update_all_sessions to management commands

--- django/clickserver/apiresult/tasks.py
+++ django/clickserver/apiresult/tasks.py
@@ -272,42 +272,6 @@ def get_session_variables(session_events,app_name):
             'product_total_price':product_total_price,'is_logged_in':is_logged_in,
             'is_paid_traffic':is_paid_traffic}
 
-@shared_task
-def update_all_user_sessions():
-    # change is_active to false if session last logged time is more than SESSION_IDLE_TIME (1 hour)
-    sessions = Sessions.objects.filter(is_active=True)
-    current_time = datetime.now()
-    for session in sessions:
-        if (current_time - session.session_end).total_seconds() > (SESSION_IDLE_TIME*60):
-            session.is_active = False
-            # update session duration
-            session.session_duration = (session.session_end - session.session_start).total_seconds()
-            session.save()
-            # update the user attributes
-            user = session.user
-            # excluding the current session
-            previous_4_sessions = Sessions.objects.filter(user=user).order_by('-session_end')[1:5]
-            previous_session = None
-            if len(previous_4_sessions) >= 1:
-                previous_session = previous_4_sessions[0] #most recent session excluding the current session
-           
-            purchase_history = [session.has_purchased for session in previous_4_sessions]
-            user.purchase_last_4_sessions = 1 if sum(purchase_history) > 0 else 0
-            user.carted_last_4_sessions = 1 if sum([session.has_carted for session in previous_4_sessions]) > 0 else 0
-            
-            # if previous session then assign purchase_prev_session to has_purchased of previous session else assign 0
-            if previous_session:
-                user.purchase_prev_session = previous_session.has_purchased
-
-            # number of sessions last 30 days
-            sessions_last_30_days = Sessions.objects.filter(user=user).filter(logged_time__gte=current_time - timedelta(days=30))
-            user.num_sessions_last_30_days = len(sessions_last_30_days)
-
-            # number of sessions last 7 days
-            sessions_last_7_days = Sessions.objects.filter(user=user).filter(logged_time__gte=current_time - timedelta(days=7))
-            user.num_sessions_last_7_days = len(sessions_last_7_days)
-            user.save()
-
 
 @shared_task
 def update_database_chunk(start_time, end_time, app_name, events_data):
